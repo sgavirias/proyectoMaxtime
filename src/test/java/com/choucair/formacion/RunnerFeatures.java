6e6d4a50ac59345144c8046f36c9100f45de3e0d
Santiago Gaviria Sanchez <sgavirias@choucairtesting.com>
2019-10-09 08:31:21 -0500

    Santiago

---
+++ src/test/java/com/choucair/formacion/RunnerFeatures.java
@@ -0,0 +1,14 @@
+package com.choucair.formacion;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+
+@RunWith(CucumberWithSerenity.class)
+//@CucumberOptions(features ="src/test/resources/features/")
+@CucumberOptions(features ="src/test/resources/features/regdoctor.Feature")
+//@CucumberOptions(features ="src/test/resources/features/ColorLib/nombre.feature")
+public class RunnerFeatures {
+
+}
