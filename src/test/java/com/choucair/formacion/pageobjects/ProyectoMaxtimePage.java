2460777904182663160a912fcf9802873a0c98bc
Santiago Gaviria Sanchez <sgavirias@choucairtesting.com>
2019-10-10 08:27:37 -0500

    Santiago

--- src/test/java/com/choucair/formacion/pageobjects/ProyectoMaxtimePage.java
+++ src/test/java/com/choucair/formacion/pageobjects/ProyectoMaxtimePage.java
@@ -6,6 +6,8 @@ import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 import org.openqa.selenium.By;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 @DefaultUrl("http://operacion.choucairtesting.com/MaxtimeChc/Login.aspx?ReturnUrl=%2fMaxtimeChc")
@@ -29,6 +31,7 @@ public class ProyectoMaxtimePage extends PageObject {
     WebElementFacade btnNuevo;
 
     @FindBy(xpath = "//td[@id='Vertical_v8_MainLayoutEdit_xaf_l128_xaf_dviProyecto_Edit_Find_B']")
+                     //td[@id='Vertical_v11_MainLayoutEdit_xaf_l135_xaf_dviProyecto_Edit_Find_B']
     WebElementFacade btnProyecto;
 
     @FindBy(xpath = "//*[contains(@id, 'Dialog_v9_LE_v10_DXSelBtn1_D')]")
@@ -37,11 +40,38 @@ public class ProyectoMaxtimePage extends PageObject {
     @FindBy(xpath = "//div[@id='Dialog_actionContainerHolder_Menu_DXI0_T']")
     WebElementFacade btnAceptar;
 
-    @FindBy(xpath = "//input[@id='Dialog_SearchActionContainer_Menu_ITCNT0_xaf_a0_Ed_I']")
-    WebElementFacade lblSearch;
+    //@FindBy(xpath = "//input[@id='Dialog_SearchActionContainer_Menu_ITCNT0_xaf_a0_Ed_I']")
+    //WebElementFacade lblSearch;
 
-    @FindBy(xpath =  "//*[contains(@id,'PopupWindowCallbackPanel_PopupWindow64692086_CIF-1')]")
-    WebElementFacade iFrame;
+    //@FindBy(xpath =  "//*[contains(@id,'PopupWindowCallbackPanel_PopupWindow64692086_CIF-1')]")
+    //WebElementFacade iFrame;
+
+    @FindBy(xpath = "//td[@id='Vertical_v8_MainLayoutEdit_xaf_l148_xaf_dviTipoHora_Edit_DD_B-1']//table[@class='dxbebt']")
+    WebElementFacade btnTipoHora;
+
+    @FindBy(xpath = "//td[@id='Vertical_v8_MainLayoutEdit_xaf_l148_xaf_dviTipoHora_Edit_DD_DDD_L_LBI3T0']")
+    WebElementFacade listTipoHora;
+
+    @FindBy(xpath = "//div[@id='Vertical_v8_MainLayoutEdit_xaf_l153_xaf_dviServicio_Edit_Find_CD']")
+    WebElementFacade btnServicio;
+
+    @FindBy(xpath = "//input[@id='Dialog_SearchActionContainer_Menu_ITCNT0_xaf_a0_Ed_I']")
+    WebElementFacade lblTxtSearch;
+
+    @FindBy(xpath = "//td[@id='Dialog_v11_LE_v12_tccell0_0']")
+    WebElementFacade btnNombServicio;
+
+    @FindBy(xpath = "//td[@id='Vertical_v8_MainLayoutEdit_xaf_l158_xaf_dviActividad_Edit_DD_B-1']")
+    WebElementFacade btnActividad;
+
+    @FindBy(xpath = "//td[@id='Vertical_v8_MainLayoutEdit_xaf_l158_xaf_dviActividad_Edit_DD_DDD_L_LBI3T0']")
+    WebElementFacade listActividad;
+
+    @FindBy(xpath = "//table[@id='Vertical_v8_MainLayoutEdit_xaf_l182_xaf_dviHoras_Edit']//td[@class='dxic']")
+    WebElementFacade lblHorasEjecutadas;
+
+    @FindBy(xpath = "//table[@id='Vertical_v8_MainLayoutEdit_xaf_l207_xaf_dviComentario_Edit']//td[@class='dxic']")
+    WebElementFacade lblComentario;
 
     public void login() {
 
@@ -56,22 +86,38 @@ public class ProyectoMaxtimePage extends PageObject {
     }
 
     public void informacion() {
+
+        WebDriverWait iframe = new WebDriverWait(getDriver(), 5);
+
         Actions act = new Actions(getDriver());
         waitFor(2).seconds();
         act.moveToElement(btnFecha).click().perform();
         act.moveToElement(btnNuevo).click().perform();
         act.moveToElement(btnProyecto).click().perform();
-        //waitFor(2).seconds();
-        //getDriver().switchTo().frame(iFrame);
-        getDriver().switchTo().frame("PopupWindowCallbackPanel_PopupWindow64692086_CIF-1");
-        $("//div[@id='Dialog_actionContainerHolder_Menu_DXI1_T']").click();
-
-        /*act.moveToElement(lblSearch).click().perform();
-        lblSearch.sendKeys("CHM2");*/
 
 
-        /*act.moveToElement(checkCodigoCliente).click().perform();
-        act.moveToElement(btnAceptar).click().perform();*/
+        iframe.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("(//iframe)[1]")));
+        //getDriver().switchTo().frame("PopupWindowCallbackPanel_PopupWindow64692086_CIF-1");
 
+
+        act.moveToElement(checkCodigoCliente).click().perform();
+        act.moveToElement(btnAceptar).click().perform();
+
+        act.moveToElement(btnTipoHora).click().perform();
+        act.moveToElement(listTipoHora).click().perform();
+
+        act.moveToElement(btnServicio).click().perform();
+        iframe.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath("(//iframe)[1]")));
+
+        lblTxtSearch.typeAndEnter("clá");
+        act.moveToElement(btnNombServicio).click().perform();
+        waitFor(10).seconds();
+        btnActividad.click();
+        //act.moveToElement(btnActividad).click().perform();
+        act.moveToElement(listActividad).click().perform();
+
+        act.moveToElement(lblHorasEjecutadas).click().sendKeys("9").perform();
+
+        act.moveToElement(lblComentario).click().sendKeys("Práctica automatización").perform();
     }
 }
